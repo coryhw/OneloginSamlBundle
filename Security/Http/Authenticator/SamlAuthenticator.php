72335594c1a478b7b48fd62dcbbe86fc896d7791
Cory Hekimian-Williams <cory@studioxmedia.com>
2022-05-13 09:05:10 -0400

    added a bunch of error log messages

--- Security/Http/Authenticator/SamlAuthenticator.php
+++ Security/Http/Authenticator/SamlAuthenticator.php
@@ -68,12 +68,14 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
 
     public function supports(Request $request): ?bool
     {
+        error_log('checking supports');
         return $request->isMethod('POST')
             && $this->httpUtils->checkRequestPath($request, $this->options['check_path']);
     }
 
     public function start(Request $request, ?AuthenticationException $authException = null): Response
     {
+        error_log('called start');
         return new RedirectResponse($this->httpUtils->generateUri($request, $this->options['login_path']));
     }
 
@@ -87,11 +89,12 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
         if ($this->options['require_previous_session'] && !$request->hasPreviousSession()) {
             throw new SessionUnavailableException('Your session has timed out, or you have disabled cookies.');
         }
-
+        error_log('pre process response');
         $this->oneLoginAuth->processResponse();
-
+        error_log('post process response');
         if ($this->oneLoginAuth->getErrors()) {
             $errorReason = $this->oneLoginAuth->getLastErrorReason();
+            error_log('Postprocess error: ' . $errorReason);
             if (null !== $this->logger) {
                 $this->logger->error($errorReason);
             }
@@ -103,6 +106,7 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
 
     public function createAuthenticatedToken(PassportInterface $passport, string $firewallName): TokenInterface
     {
+        error_log('creating authenticated token');
         if (!$passport instanceof Passport) {
             throw new LogicException(sprintf('Passport should be an instance of "%s".', Passport::class));
         }
@@ -112,62 +116,73 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
 
     public function createToken(Passport $passport, string $firewallName): TokenInterface
     {
+        error_log('creating token');
         if (!$passport->hasBadge(SamlAttributesBadge::class)) {
             throw new LogicException(sprintf('Passport should contains a "%s" badge.', SamlAttributesBadge::class));
         }
 
         /** @var SamlAttributesBadge $badge */
         $badge = $passport->getBadge(SamlAttributesBadge::class);
-
+        error_log('got passport badge');
         return new SamlToken($passport->getUser(), $firewallName, $passport->getUser()->getRoles(), $badge->getAttributes());
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        error_log('onauth success');
         return $this->successHandler->onAuthenticationSuccess($request, $token);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
+        error_log('onauth failure ' . $exception->getMessage());
         return $this->failureHandler->onAuthenticationFailure($request, $exception);
     }
 
     protected function createPassport(): Passport
     {
         $attributes = $this->extractAttributes();
+        error_log('extracted attributes');
         $username = $this->extractUsername($attributes);
-
+        error_log('extracted username ' . $username);
         $userBadge = new UserBadge(
             $username,
             function ($identifier) use ($attributes) {
                 try {
+                    error_log('loading userbadge');
                     $user = $this->userProvider->loadUserByIdentifier($identifier);
+                    error_log('loaded userbadge');
                 } catch (UserNotFoundException $exception) {
+                    error_log('loading userbadge error ' . $exception->getMessage());
                     if (!$this->userFactory instanceof SamlUserFactoryInterface) {
                         throw $exception;
                     }
-
+                    error_log('creating new user');
                     $user = $this->generateUser($identifier, $attributes);
                 } catch (\Throwable $exception) {
+                    error_log('creating user error ' . $exception->getMessage());
                     throw new AuthenticationException('The authentication failed.', 0, $exception);
                 }
 
                 if ($user instanceof SamlUserInterface) {
+                    error_log('setting saml attributes');
                     $user->setSamlAttributes($attributes);
                     if ($this->eventDispatcher) {
+                        error_log('dispatching usermodified event');
                         $this->eventDispatcher->dispatch(new UserModifiedEvent($user));
                     }
                 }
-
+                error_log('finishing userbadge creation');
                 return $user;
             }
         );
-
+        error_log('building selfvalidating passport');
         return new SelfValidatingPassport($userBadge, [new SamlAttributesBadge($attributes)]);
     }
 
     protected function extractAttributes(): array
     {
+        error_log('extracting attributes');
         if (isset($this->options['use_attribute_friendly_name']) && $this->options['use_attribute_friendly_name']) {
             $attributes = $this->oneLoginAuth->getAttributesWithFriendlyName();
         } else {
@@ -180,6 +195,7 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
 
     protected function extractUsername(array $attributes): string
     {
+        error_log('extracting username');
         if (isset($this->options['username_attribute'])) {
             if (!\array_key_exists($this->options['username_attribute'], $attributes)) {
                 if (null !== $this->logger) {
@@ -196,6 +212,7 @@ class SamlAuthenticator implements AuthenticatorInterface, AuthenticationEntryPo
 
     protected function generateUser(string $username, array $attributes): UserInterface
     {
+        error_log('generating user function');
         $user = $this->userFactory->createUser($username, $attributes);
 
         if ($this->eventDispatcher) {
